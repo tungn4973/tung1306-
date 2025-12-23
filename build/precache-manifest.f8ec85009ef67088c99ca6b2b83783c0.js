b364b0604491deb9588fd5723d2a4930ad083acb
tungn4973 <tungn4973@gmail.com>
2025-12-23 16:22:20 +0700

    fix

---
+++ build/precache-manifest.f8ec85009ef67088c99ca6b2b83783c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a842294a03da0f4c8a8c484ba964390a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a174f83df91dff4ae833",
+    "url": "/static/css/main.b88ec630.chunk.css"
+  },
+  {
+    "revision": "c97fd279e172af7af6b0",
+    "url": "/static/js/2.701f22b9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.701f22b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a174f83df91dff4ae833",
+    "url": "/static/js/main.39973089.chunk.js"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
